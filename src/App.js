76af86cadaabac3c424a0cad45aa9a8ed668d740
yuntaek ha <yuntaek.ha@gmail.com>
2021-02-28 03:02:15 +0900

    add validator

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
 import React, { useState } from "react";
 import "./App.css";
 
-const useInput = (initialValue) => {
+const useInput = (initialValue, validator) => {
   const [value, setValue] = useState(initialValue);
   const onChange = (event) => {
-    console.log(event);
+    const {
+      target: { value },
+    } = event;
+    let willUpdate = true;
+    if (typeof validator === "function") {
+      willUpdate = validator(value);
+    }
+    if (willUpdate) {
+      setValue(value);
+    }
   };
   return { value, onChange };
 };
 
 const App = () => {
-  const name = useInput("Mr.");
+  const maxLen = (value) => !value.includes("@");
+  const name = useInput("Mr.", maxLen);
   return (
     <div className="App">
       <h1>Hello</h1>
-      <input placeholder="Name" value={name.value} onChange={name.onChange} />
+      <input placeholder="Name" {...name} />
     </div>
   );
 };
